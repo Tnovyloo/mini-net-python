47a147e75fa597f9b609e77046f2e8d6e84cc88a
tnovyloo <loobrome@gmail.com>
2023-06-19 12:13:40 +0200

    Reworking project.

--- test.py
+++ test.py
@@ -12,30 +12,37 @@ from mininet.node import Controller
 import networkx as nx
 import matplotlib.pyplot as plt
 
+
+
+class NetworkTopoPythonAPI(Topo):
+
+    def build(self, **opts):
+        s1 = self.addSwitch('s1', failMode='standalone')
+        s2 = self.addSwitch('s2', failMode='standalone')
+
+
+        # Adding hosts
+        d1 = self.addHost('d1', ip='192.168.0.1/28')
+        d2 = self.addHost('d2', ip='192.168.0.2/28')
+        d3 = self.addHost('d3', ip='192.168.0.3/28')
+        d4 = self.addHost('d4', ip='192.168.0.9/28')
+        d5 = self.addHost('d5', ip='192.168.0.10/28')
+        d6 = self.addHost('d6', ip='192.168.0.11/28')
+
+        d7 = self.addHost('d7', ip='192.168.0.12/28')
+        self.addLink(d7, s2)
+
+        # Connecting hosts to switches
+        for d, s in [(d1, s1), (d2, s1), (d3, s1)]:
+            self.addLink(d, s)
+        for d, s in [(d4, s1), (d5, s1), (d6, s1)]:
+            self.addLink(d, s)
+
+
 class NetworkTopo(Topo):
     # Builds network topology
     def build(self, **_opts):
 
-        # s1 = self.addSwitch('s1', failMode='standalone')
-        s2 = self.addSwitch('s2', failMode='standalone')
-
-        #
-        # # Adding hosts
-        # d1 = self.addHost('d1', ip='192.168.0.1/28')
-        # d2 = self.addHost('d2', ip='192.168.0.2/28')
-        # d3 = self.addHost('d3', ip='192.168.0.3/28')
-        # d4 = self.addHost('d4', ip='192.168.0.9/28')
-        # d5 = self.addHost('d5', ip='192.168.0.10/28')
-        # d6 = self.addHost('d6', ip='192.168.0.11/28')
-        #
-        # d7 = self.addHost('d7', ip='192.168.0.12/28')
-        # self.addLink(d7, s2)
-        #
-        # # Connecting hosts to switches
-        # for d, s in [(d1, s1), (d2, s1), (d3, s1)]:
-        #     self.addLink(d, s)
-        # for d, s in [(d4, s1), (d5, s1), (d6, s1)]:
-        #     self.addLink(d, s)
         choice = 0
         switch_list = []
 
@@ -104,23 +111,65 @@ class NetworkTopo(Topo):
                 plt.savefig(f"{file_name}.jpg", dpi=300)
 
 def run():
-    topo = NetworkTopo()
 
-    # choice = input("Visualize topology to jpg? (y/n): ")
-    # if choice.lower() == "y":
-    #     # Exporting mininet topology to list of edges by .links() function, and creating it in NetworkX
-    #     # https://github.com/bigswitch/mininet/blob/master/mininet/topo.py
-    #
-    #     topo_links = topo.links()
-    #     graph = nx.Graph()
-    #     graph.add_edges_from(topo_links)
-    #
-    #     # Saving graph to file
-    #     file_name = input("Type filename: ")
-    #     nx.draw(graph, with_labels=True)
-    #     plt.savefig(f"{file_name}.jpg", dpi=300)
+    choice = int(input("If you want to run our CLI type 1\n"
+                       "If you want to run created topology by Python API type 2\n"
+                       "If you want to put Mininet Dump and Mininet Links type 3\n"))
 
-    # To run Mininet uncomment below code.
+
+    if choice == 1:
+        topo = NetworkTopo()
+
+    if choice == 2:
+        topo = NetworkTopoPythonAPI()
+
+        choice = input("Visualize topology to jpg? (y/n): ")
+        if choice.lower() == "y":
+            # Exporting mininet topology to list of edges by .links() function, and creating it in NetworkX
+            # https://github.com/bigswitch/mininet/blob/master/mininet/topo.py
+
+
+            topo_links = topo.links()
+            print(topo_links)
+            graph = nx.Graph()
+            graph.add_edges_from(topo_links)
+
+            # Saving graph to file
+            file_name = input("Type filename: ")
+            nx.draw(graph, with_labels=True)
+            plt.savefig(f"{file_name}.jpg", dpi=300)
+
+    if choice == 3:
+
+
+        with open("MininetLinks.txt", mode="r") as file:
+            mininet_raw_links = [line.rsplit() for line in file]
+
+        if mininet_raw_links is None:
+            print("Please paste 'mininet links' from mininet into Mininetlinks.txt")
+            return None
+
+
+        mininet_links = []
+        # for link in mininet_raw_links:
+        for raw_link in mininet_raw_links:
+            link = raw_link[0].split("<->")
+            e1 = link[0].split('-')[0]
+            e2 = link[1].split('-')[0]
+            mininet_links.append((e1, e2))
+
+        # print(mininet_links)
+
+        graph = nx.Graph()
+        graph.add_edges_from(mininet_links)
+
+        # Saving graph to file
+        file_name = input("Type filename: ")
+        nx.draw(graph, with_labels=True)
+        plt.savefig(f"{file_name}.jpg", dpi=500)
+
+
+    # To run Mininet CLI uncomment below code.
 
     # net = Mininet(topo=topo, controller=None)
     # net.start()
